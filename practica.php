ab955436e773d48f5432352aacf3be884c0217aa
Josmar <josmardbd@gmail.com>
2020-04-02 03:43:35 -0430

    Segundo video

--- practica.php
+++ practica.php
@@ -1,6 +1,50 @@
 <?php
+
 //Video 1
+echo"Practica 1";
+
+echo"<br>";
+echo"<br>";
+
 echo("Hola Mundo desde PHP");
+
+//Video 2
+echo"<br>";
+echo"<br>";
+
+echo"Practica 2";
+
+$edad = 17;
+
+//PHP es sencible a mayusculas y minusculas por ejemplo si escribimos el mismo nombre pero con una mayuscula
+
+$Edad = 18; //es diferente
+echo"<br>";//salto de linea
+echo"<br>";
+
+echo $edad;
+
+echo"<br>";
+echo"<br>";
+
+echo $Edad;
+
+
+echo"<br>";
+echo"<br>";
+
+//Concatenacion
+
+echo"Mi edad es:  ".$edad;
+
+
+echo"<br>";
+echo"<br>";
+ 
+$nombre = "Josmar";//<-- Declaracion de variable de tipo string
+
+
+echo $nombre .  "tiene la edad de ".$edad."años"; 
 
 
 
